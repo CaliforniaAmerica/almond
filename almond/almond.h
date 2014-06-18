7525869318f745ef4a455a3de75ea5a2672e0414
lost.Fox L <lost.Fox2012+gh@gmail.com>
2014-06-18 17:36:57 +0800

    fix log

--- almond/almond.h
+++ almond/almond.h
@@ -15,19 +15,26 @@ using namespace std;
 extern "C" __declspec(dllexport) void initMod(void);
 
 /*!
-	@brief a printf like log, use g2log
+	@brief a light printf like log, use g2log
 	@see http://www.codeproject.com/Articles/288827/g-log-An-efficient-asynchronous-logger-using-Cplus
 
-	@param iLevel 3	FATAL
-	@param iLevel 2	WARNING
-	@param iLevel 1	INFO
-	@param iLevel 0	DEBUG
+	@param iLevel == 3	FATAL
+	@param iLevel == 2	WARNING
+	@param iLevel == 1	INFO
+	@param iLevel == 0	DEBUG
 
-	@param pcFormat Format control, only recognise one char 4 now
+	@param pcFormat	Format control, 
+		support numbers: %d, %x, %f %u; strings: %s; points: %p
+	
 	@argument Optional arguments. 
+
+	@UNDONE log point
 
  */
 extern "C" __declspec(dllexport) void almondLog(int iLevel, const char* pcFormat, ...);
+
+///test4more format
+extern "C" __declspec(dllexport) void alogf(int iLevel, const char* pcFormat, ...);
 
 //TODO log stream
 
